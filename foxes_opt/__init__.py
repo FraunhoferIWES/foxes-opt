cd2ea87adc89ff6e2ed763218fc1ff2ebc92f822
Jonas Schmidt <jonas.schmidt@iwes.fraunhofer.de>
2024-07-02 21:49:29 +0200

    adding docs

--- foxes_opt/__init__.py
+++ foxes_opt/__init__.py
@@ -2,8 +2,17 @@
 Wind farm optimization.
 """
 
-from .core import FarmOptProblem, FarmObjective, FarmConstraint
+from .core import FarmOptProblem, FarmObjective, FarmConstraint # noqa: F401
 
-from . import problems
-from . import constraints
-from . import objectives
+from . import problems # noqa: F401
+from . import constraints # noqa: F401
+from . import objectives # noqa: F401
+
+try:
+    from importlib.resources import files
+
+    __version__ = files(__package__).joinpath("VERSION").read_text()
+except ImportError:
+    from importlib.resources import read_text
+
+    __version__ = read_text(__package__, "VERSION")
