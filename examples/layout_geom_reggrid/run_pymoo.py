be7eb02c9506dde56ccc9aff975505aff96e13cb
Jonas Schulte <jonas.schulte@iwes.fraunhofer.de>
2025-09-06 14:25:41 +0200

    cleaning up a bit

---
+++ examples/layout_geom_reggrid/run_pymoo.py
@@ -0,0 +1,111 @@
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+from iwopy.interfaces.pymoo import Optimizer_pymoo
+
+import foxes
+import foxes_opt.problems.layout.geom_layouts as grg
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--min_dist",
+        help="Minimal turbine distance in m",
+        type=float,
+        default=200.0,
+    )
+    parser.add_argument(
+        "-N", "--n_turbines", help="Maximal number of turbines", type=int, default=200
+    )
+    parser.add_argument(
+        "-nop", "--no_pop", help="Switch off vectorization", action="store_true"
+    )
+    parser.add_argument(
+        "-A", "--opt_algo", help="The pymoo algorithm name", default="GA"
+    )
+    parser.add_argument(
+        "-P", "--n_pop", help="The population size", type=int, default=100
+    )
+    parser.add_argument(
+        "-G", "--n_gen", help="The number of generations", type=int, default=150
+    )
+    parser.add_argument("-e", "--engine", help="The engine", default="process")
+    parser.add_argument(
+        "-n", "--n_cpus", help="The number of cpus", default=None, type=int
+    )
+    parser.add_argument(
+        "-c",
+        "--chunksize_states",
+        help="The chunk size for states",
+        default=None,
+        type=int,
+    )
+    parser.add_argument(
+        "-C",
+        "--chunksize_points",
+        help="The chunk size for points",
+        default=None,
+        type=int,
+    )
+    args = parser.parse_args()
+
+    boundary = (
+        foxes.utils.geom2d.ClosedPolygon(
+            np.array(
+                [[0, 1000], [400, 1600], [2500, 1100], [2200, 300]], dtype=np.float64
+            )
+        )
+        + foxes.utils.geom2d.Circle([2500.0, 0.0], 500.0)
+        + foxes.utils.geom2d.ClosedPolygon(
+            np.array(
+                [[2200, 0], [2200, 2600], [2800, 2600], [2800, 0]], dtype=np.float64
+            )
+        )
+    )
+
+    with foxes.Engine.new(
+        engine_type=args.engine,
+        n_procs=args.n_cpus,
+        chunk_size_states=args.chunksize_states,
+        chunk_size_points=args.chunksize_points,
+        verbosity=0,
+    ):
+        problem = grg.GeomRegGrid(
+            boundary, 
+            n_turbines=args.n_turbines,
+            min_dist=args.min_dist
+        )
+        problem.add_objective(grg.OMaxN(problem))
+        problem.initialize()
+
+        fig = problem.get_fig().get_figure()
+        plt.show()
+        plt.close(fig)
+
+        solver = Optimizer_pymoo(
+            problem,
+            problem_pars=dict(
+                vectorize=not args.no_pop,
+            ),
+            algo_pars=dict(
+                type=args.opt_algo,
+                pop_size=args.n_pop,
+                seed=None,
+            ),
+            setup_pars=dict(),
+            term_pars=("n_gen", args.n_gen),
+        )
+        solver.initialize()
+        solver.print_info()
+
+        results = solver.solve()
+        solver.finalize(results)
+
+        print()
+        print(results)
+
+        xy, valid = results.problem_results
+        fig = problem.get_fig(xy, valid).get_figure()
+        plt.show()
+        plt.close(fig)
